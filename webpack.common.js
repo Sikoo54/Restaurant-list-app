ef5262c43629912299d48176166111ef5bdb88b1
Sikoo54 <sachas4f@gmail.com>
2024-11-25 20:03:45 +0800

    kriteria wajib done

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -39,6 +40,30 @@ module.exports = {
         {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
+        },
+      ],
+    }),
+
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: './sw.bundle.js',
+      runtimeCaching: [
+        {
+          urlPattern: ({ url }) =>
+            url.href.startsWith('https://restaurant-api.dicoding.dev/list'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'FoodHunter-api',
+          },
+        },
+        {
+          urlPattern: ({ url }) =>
+            url.href.startsWith(
+              'https://restaurant-api.dicoding.dev/images/large'
+            ),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'FoodHunter-image-api',
+          },
         },
       ],
     }),
